a05ce884b05c512595d9400f4a7294a3ce2e8370
TTKHScriptPoetry <kimdotnetpossible@att.net>
2022-03-26 04:27:53 -0500

    API POST, PUT, and DELETE routes for Tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,24 +55,23 @@ router.post('/', (req, res) => {
   // create a new category
   Category.create({
     category_name: req.body.category_name
- })
+  })
  .then(dbCategoryData => res.json(dbCategoryData))
  .catch(err => {
     console.log(err);
     res.status(500).json(err);
-});
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update({
-      category_name: req.body.category_name 
-    },
-    {
-      where: {
-        id: req.params.id
-      }
-    })
+  Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where:{id: req.body.id}
+      })
     .then(dbCategoryData => {
       if (!dbCategoryData) {
       res.status(404).json({ message: 'No category found with this id' });
